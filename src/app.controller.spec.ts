e921eeeadc3edb7f4fc36e8353c7e7e9554fe314
mayska <alexandre.meunier31@gmail.com>
2022-12-10 19:28:24 +0100

    init api 1

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppService } from './app.service'; 
 import { PrismaService } from './prisma/prisma.service';
 
 describe('AppController', () => {
